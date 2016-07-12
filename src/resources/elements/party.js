c435feebdbe393f7ce71424b7a1c08963371d494
Harro Lissenberg <hlissenberg@qualogy.com>
2016-07-12 21:43:02 +0200

    After custom element examples

---
+++ src/resources/elements/party.js
@@ -0,0 +1,10 @@
+export class Party {
+  date = new Date();
+  guests = ['Harro', 'Rob', 'Charles'];
+
+  constructor() {
+    setInterval(() => {
+      this.date = new Date()
+    }, 5000)
+  }
+}
